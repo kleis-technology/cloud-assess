ee9257b9501e02fa2b3f4073fa314cbde4fb395e
Peva Blanchard <pblanchard@kleis.ch>
2024-04-15 18:43:28 +0200

    build.gradle: removed duplicate dep

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,6 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-json")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
