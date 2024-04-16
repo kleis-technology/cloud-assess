9e2a1eeeeeba241582b5847fa28ddddf59d00a62
Peva Blanchard <pblanchard@kleis.ch>
2024-04-16 17:09:05 +0200

    build gradle spring boot bump version 3.2.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     id("maven-publish")
-    id("org.springframework.boot") version "3.1.3"
+    id("org.springframework.boot") version "3.2.4"
     id("io.spring.dependency-management") version "1.1.3"
     kotlin("jvm") version "1.8.22"
     kotlin("plugin.spring") version "1.8.22"
