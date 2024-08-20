00fbb935dad28358474c42a08051846f0e135c57
Peva Blanchard <pblanchard@kleis.ch>
2024-08-21 00:45:25 +0200

    lcaac bump version 1.7.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 }
 
 group = "org.cloud_assess"
-version = "1.7.0"
+version = "1.7.1"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_17
@@ -30,7 +30,7 @@ repositories {
 }
 
 dependencies {
-    val lcaacVersion = "1.7.0"
+    val lcaacVersion = "1.7.1"
     implementation("ch.kleis.lcaac:core:$lcaacVersion")
     implementation("ch.kleis.lcaac:grammar:$lcaacVersion")
 
