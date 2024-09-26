b863e23c517718df95b61b39e2e62ab96c78e36d
Peva Blanchard <pblanchard@kleis.ch>
2024-09-26 19:41:42 +0200

    trace: basic error handling

--- src/main/kotlin/org/cloud_assess/controller/TraceController.kt
+++ src/main/kotlin/org/cloud_assess/controller/TraceController.kt
@@ -1,11 +1,14 @@
 package org.cloud_assess.controller
 
+import ch.kleis.lcaac.core.lang.evaluator.EvaluatorException
 import org.cloud_assess.api.DebugApi
+import org.cloud_assess.dto.ErrorDto
 import org.cloud_assess.dto.TraceRequestListDto
 import org.cloud_assess.dto.TraceResponseListDto
 import org.cloud_assess.service.MapperService
 import org.cloud_assess.service.debug.TraceService
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -17,5 +20,18 @@ class TraceController(
         val analysis = traceService.analyze(traceRequestListDto)
         val outputDto = mapperService.map(analysis)
         return ResponseEntity.ok(outputDto)
+    }
+
+    @ExceptionHandler(value = [IllegalArgumentException::class, EvaluatorException::class])
+    fun handleException(exception: Exception): ResponseEntity<ErrorDto> {
+        val errorDto = ErrorDto(
+            message = exception.message ?: "unknown"
+        )
+        val code = when (exception) {
+            is IllegalArgumentException -> 400
+            is EvaluatorException -> 500
+            else -> 500
+        }
+        return ResponseEntity.status(code).body(errorDto)
     }
 }
