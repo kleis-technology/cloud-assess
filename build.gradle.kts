79e7fdcf1c1de8c76c272ddd0d000616865d7ecb
Peva Blanchard <pblanchard@kleis.ch>
2025-08-20 16:44:22 +0200

    lcaac bump version 1.8.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ repositories {
 }
 
 dependencies {
-    val lcaacVersion = "1.7.13"
+    val lcaacVersion = "1.8.0"
     implementation("ch.kleis.lcaac:core:$lcaacVersion")
     implementation("ch.kleis.lcaac:grammar:$lcaacVersion")
 
